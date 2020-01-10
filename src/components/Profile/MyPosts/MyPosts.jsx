337f4255e1c0a04c1bdda7af074a7306641d7d7d
Mathew <m.baukov@bk.ru>
2020-01-10 15:27:07 +0300

    pureComppnent memo componentShouldUpdate

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { Component, PureComponent } from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/profileReducer';
 import { Field, reduxForm } from 'redux-form';
-import {required, maxLengthCreator, test} from '../../../utils/validators/validators';
-import {Textarea} from '../../common/FromsControls/FormControls';
+import { required, maxLengthCreator, test } from '../../../utils/validators/validators';
+import { Textarea } from '../../common/FromsControls/FormControls';
 
 
-const MyPosts = (props) => {
+const MyPosts = React.memo( props => {
   let componentsPost = props.posts.map(post => <Post message={post.post} id={post.id} key={post.id} url_image={post.photo_url} likesCount={post.likes} />);
 
   let createNewWallPost = (formData) => {
@@ -15,6 +15,7 @@ const MyPosts = (props) => {
     //props.updateNewPostText(formData.wallPostText);
     props.addPost(formData.wallPostText);
   }
+  console.log('render');
 
   return (
     <div className={s.mypostMainWrapp}>
@@ -27,16 +28,44 @@ const MyPosts = (props) => {
       </div>
     </div>
   );
-}
+});
+
+
+/*
+class MyPosts extends PureComponent {
+  render() {
+
+    console.log('RENDER');
+    let componentsPost = this.props.posts.map(post => <Post message={post.post} id={post.id} key={post.id} url_image={post.photo_url} likesCount={post.likes} />);
+
+    let createNewWallPost = (formData) => {
+      console.log(formData);
+      //props.updateNewPostText(formData.wallPostText);
+      this.props.addPost(formData.wallPostText);
+    }
+    return (
+      <div className={s.mypostMainWrapp}>
+        <h3>My posts</h3>
+        <div>
+          <WallPostFormRedux onSubmit={createNewWallPost} />
+        </div>
+        <div className={s.postWrapper}>
+          {componentsPost}
+        </div>
+      </div>
+    );
+  }
+}*/
+
 
 const maxLength5 = maxLengthCreator(5);
 
 
 const WallPostForm = (props) => {
   return (
-    <form onSubmit = {props.handleSubmit}>
+    <form onSubmit={props.handleSubmit}>
       <div>
-        <Field type="text" placeholder='АУе челяба' name='wallPostText' component={Textarea} validate = {[required, maxLength5]}/>
+        <Field type="text" placeholder='АУе челяба' name='wallPostText' component={Textarea} validate={[required, maxLength5]} />
       </div>
       <div>
         <button>Add</button>
@@ -45,6 +74,6 @@ const WallPostForm = (props) => {
   );
 }
 
-const WallPostFormRedux = reduxForm({form: 'addWallPost'})(WallPostForm);
+const WallPostFormRedux = reduxForm({ form: 'addWallPost' })(WallPostForm);
 
 export default MyPosts;
