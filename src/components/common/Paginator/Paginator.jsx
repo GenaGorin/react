38d15d9a538cf32a6e5aa8211351885325f676c0
Mathew <m.baukov@bk.ru>
2020-01-18 15:00:58 +0300

    update paginator

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './Paginator.module.css';
 
 let Paginator = (props) => {
+    let portionSize = 30;
     let pageCount = Math.ceil(props.totalUserCount / props.pageSize);
     let pages = [];
+    let allPortions = Math.ceil(pageCount / portionSize);
     for (let index = 1; index <= pageCount; index++) {
         pages.push(index);
 
     }
+
+    let [portionNum, changePortion] = useState(1);
+
+    let setPortion = (currentPort) => {
+        changePortion(currentPort);
+    }
+
+    let start = (portionNum - 1) * portionSize + 1;
+    let stop = portionNum * portionSize;
+
     return (
-            <div>
-                {pages.map(page => {
-                    return <span key = {page} onClick={(e) => { props.onPageChanged(page) }} className={props.currentPage === page ? s.selectedPage : 'dick'}>{page}</span>
-                })}
-            </div>
+        <div>
+            {portionNum > 1 ? <button onClick={() => { setPortion(portionNum - 1) }}>Prev</button> : ''}
+            {pages.map(page => {
+                if (page >= start && page <=stop) {
+                    return <span key={page} onClick={(e) => { props.onPageChanged(page) }} className={props.currentPage === page ? s.selectedPage : s.dick}>{page}</span>   
+                }
+            })}
+            {portionNum < allPortions ? <button onClick={() => { setPortion(portionNum + 1) }}>Next</button> : ''}
+        </div>
     );
 }
 
