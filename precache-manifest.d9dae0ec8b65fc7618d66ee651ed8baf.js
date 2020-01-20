9a23e39aecb49c6c6c0114efd2e42a9362007cf8
Mathew <m.baukov@bk.ru>
2020-01-20 15:17:35 +0300

    Updates

---
+++ precache-manifest.d9dae0ec8b65fc7618d66ee651ed8baf.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630c7bb02943517084507667d0695012",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "9616557b9b0e7734a24d",
+    "url": "/react/static/css/3.f154b5f0.chunk.css"
+  },
+  {
+    "revision": "ebf84a45484c06ef277c",
+    "url": "/react/static/css/4.6e307655.chunk.css"
+  },
+  {
+    "revision": "e3596be7b261a135dd14",
+    "url": "/react/static/css/main.1979108c.chunk.css"
+  },
+  {
+    "revision": "cc9f2d152c4152a1426a",
+    "url": "/react/static/js/2.56233285.chunk.js"
+  },
+  {
+    "revision": "282644d374d35a1b5d48b26686d2547e",
+    "url": "/react/static/js/2.56233285.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9616557b9b0e7734a24d",
+    "url": "/react/static/js/3.9209bf39.chunk.js"
+  },
+  {
+    "revision": "ebf84a45484c06ef277c",
+    "url": "/react/static/js/4.a774e212.chunk.js"
+  },
+  {
+    "revision": "e3596be7b261a135dd14",
+    "url": "/react/static/js/main.8813409b.chunk.js"
+  },
+  {
+    "revision": "ed0c9c2d70f5e824d864",
+    "url": "/react/static/js/runtime-main.5b5eff1d.js"
+  },
+  {
+    "revision": "b7d803c5666301b4bd39e30b44fd84fa",
+    "url": "/react/static/media/2zH6.b7d803c5.gif"
+  },
+  {
+    "revision": "429827e685856f650825235f75142fe8",
+    "url": "/react/static/media/69880c1f5e57698.429827e6.mp3"
+  },
+  {
+    "revision": "e9e9423263ea89f06bfd73e868fa194d",
+    "url": "/react/static/media/gorin.e9e94232.jpg"
+  }
+]);
---
+++ static/js/4.a774e212.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-kabzda-1"]=this["webpackJsonpreact-kabzda-1"]||[]).push([[4],{299:function(t,e,a){t.exports={title:"News_title__3-72i"}},303:function(t,e,a){"use strict";a.r(e);var n=a(0),s=a.n(n),c=a(299),i=a.n(c),r=a(53);e.default=Object(r.a)((function(){return s.a.createElement("h1",{className:i.a.title},"Hello world it's news")}))}}]);
+//# sourceMappingURL=4.a774e212.chunk.js.map
